4cf89486577cfe1291db789f9973c94db40da8a3
AyaBalena <147692709+AyaBalena@users.noreply.github.com>
2024-01-29 15:00:47 +0800

    Create activity7.js

---
+++ activity7.js
@@ -0,0 +1,9 @@
+
+let typepass = prompt("Enter your password");
+var password = "p@ssw0rd@2023";
+
+if (password.length >= 8 && password.includes ("p@ssword")) {
+    console.log ("Valid password");
+} else {
+    console.log ("Invalid password");
+}
